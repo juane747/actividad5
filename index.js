54d3c25b03b3dc40401ce8b1302b9885666e869e
juane747 <98864349+juane747@users.noreply.github.com>
2023-03-27 00:13:53 -0600

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var botontransmitir = document.getElementById('transmitir')
+var tablaResultados = document.getElementById('TablaResultados')
+var botonGuardar = document.getElementById('guardar')
+var botonConsultar = document.getElementById('consultar')
+
+botontransmitir.addEventListener('click',function(){
+    
+    var autor= document.getElementById('nombre-autor')
+    var usoautor=autor.value
+    //alert(usoautor)
+    fetch('https://openlibrary.org/search/authors.json?q='+usoautor)
+    .then(res=>res.json())
+    .then(resJson=>{
+        var documentos = resJson['docs']
+        botonGuardar.addEventListener('click',function(){
+            window.comunicacion.guardarregistro(documentos)
+           
+        })
+        tablaResultados.innerHTML=""
+        for(let i =0; i<documentos.length;i++){
+            var obra = documentos[i]
+            tablaResultados.innerHTML += "<tr>"+
+                                                "<th scope =\"row\">"+i+"</td>"+
+                                                "<td>"+obra['key']+"</td>"+
+                                                "<td>"+obra['type']+"</td>"+
+                                                "<td>"+obra['name']+"</td>"+
+                                                "<td>"+obra['_version_']+"</td>"+
+                                            "</tr>"
+        }
+    })
+})
+
---
+++ main.js
@@ -0,0 +1,77 @@
+const{app, BrowserWindow,ipcMain}=require('electron');
+const path= require('path')
+const mysql = require('mysql2')
+
+//Conectar squelize para la base de datos
+const {Sequelize, DataTypes}=require('sequelize');
+
+//conexion a base de datos
+const sequelize=new Sequelize('bibliografia','root','THE PERFECT',{
+    host:'localhost',
+    dialect:'mysql'
+});
+//creacion de inicio sesion
+const Nombre=sequelize.define('Nombre',{
+    //Aqui definimos los atributos del modelo autor
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+     // allowNull:false si no se coloca, permite ver si es requerido o no
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    codigo:{
+        type: DataTypes.STRING,       
+    },
+    rol:{
+        type: DataTypes.STRING
+    },
+    nombre:{
+        type: DataTypes.STRING
+    },
+    version:{
+        type: DataTypes.STRING
+    },
+    fecha:{
+        type: DataTypes.DATE
+    }
+},{
+    //otras opciones del modelo aqui  timestamps, la ultima vez que modifico si la tabla no lo lleva hay que deshabilitar
+    timestamps: false  
+});
+
+//evaluacion de la conexion correcta
+sequelize.authenticate()
+.then(()=>{
+    console.log('coneccion correcta');
+})
+.catch((error)=>{
+    console.error('Error de conexion',error);
+})
+function createWindow(){
+    const venta = new BrowserWindow({
+        width:1500,
+        heigth:800,
+        webPreferences:{//nos permite accede a funciones nod, en este caso para interface de comunicacion
+            preload: path.join(app.getAppPath(),'preload.js')
+        },
+    })
+    venta.loadFile('index.html')
+}
+
+ipcMain.on("guardarregistro",function(event,args){
+    for(let i =0; i<args.length;i++){
+        var arreglo = args[i]
+        Nombre.create({
+            codigo:arreglo['key'],
+            rol:arreglo['type'],
+            nombre:arreglo['name'],
+            version:" "+arreglo['_version_']+"",
+            fecha:Date.now()
+           })
+    }
+   
+})
+
+
+app.whenReady().then(createWindow)
